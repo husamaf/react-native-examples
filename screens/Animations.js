706226fba735fdb89477d70b3a52db2fceae40e0
husamaf <husam.a.h.f@gmail.com>
2021-08-07 10:46:26 -0400

    [#ADD] Add a button

--- screens/Animations.js
+++ screens/Animations.js
@@ -3,10 +3,12 @@ import { StyleSheet, View, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
 
+const { cond, Value } = Animated;
+
 const Item = props => {
   return (
     <View style={styles.item}>
-      <Text style={{ textAlign: 'center' }}>This is an Item</Text>
+      <Text style={{ textAlign: 'center' }}>Animated View</Text>
     </View>
   );
 };
@@ -16,8 +18,11 @@ const Animations = props => {
     <View style={styles.root}>
       <Text style={styles.title}>Animations</Text>
       <Item />
-      <View>
-        <Text style={styles.conclusions}>Conclusions:</Text>
+      <TouchableOpacity style={styles.button} onPress={() => console.log('HELLO')}>
+        <Text>Press Me!</Text>
+      </TouchableOpacity>
+      <View style={styles.conclusions}>
+        <Text style={styles.conclusionsText}>Conclusions:</Text>
         <Text>Add text here</Text>
       </View>
     </View>
@@ -26,15 +31,17 @@ const Animations = props => {
 
 const styles = StyleSheet.create({
   root: { flex: 1, padding: 20, alignItems: 'center' },
-  title: { marginBottom: 100, fontWeight: 'bold' },
+  title: { margin: 50, fontWeight: 'bold' },
   item: {
     width: 100,
     borderWidth: 2,
-    marginBottom: 100,
+    margin: 50,
     padding: 5,
     backgroundColor: 'yellow'
   },
-  conclusions: { fontWeight: 'bold' }
+  button: { borderRadius: 20, margin: 50, padding: 10, backgroundColor: 'orange' },
+  conclusions: { margin: 50 },
+  conclusionsText: { fontWeight: 'bold' }
 });
 
 export default Animations;
